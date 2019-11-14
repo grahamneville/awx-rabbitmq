45cae594acb030bc49f7c597e14a0d9e414bf643
grahamneville <grahamneville@users.noreply.github.com>
2019-11-14 06:56:19 +0000

    Force boot rabbit if mnesia database already exists

--- launch.sh
+++ launch.sh
@@ -6,10 +6,14 @@ set -e
 if [ `id -u` -ge 500 ]; then
     echo "rabbitmq:x:`id -u`:`id -g`:,,,:${HOME}:/bin/ash" >> /tmp/passwd
     cat /tmp/passwd > /etc/passwd
-    rm /tmp/passwd
+   rm /tmp/passwd
 fi
 
 echo "$RABBITMQ_ERLANG_COOKIE" > $HOME/.erlang.cookie
 chmod 0600 $HOME/.erlang.cookie
+
+if [ "$RABBITMQ_NODENAME" ]; then
+    if [ -d "/var/lib/rabbitmq/mnesia/\${RABBITMQ_NODENAME}" ]; then rabbitmqctl force_boot; fi
+fi
 
 docker-entrypoint.sh rabbitmq-server
